e6766b75792737094a798fa1de627d171c519078
arman-ag <alighanbari.1994@gmail.com>
2022-04-29 16:54:10 +0430

    fix: :bug: Fix translation in about page
    add graphql object to about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,9 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import { Trans } from 'react-i18next';
-import Layout from '../components/layout';
+import Layout from '../../components/layout';
 
-export default function Home() {
+const about = () => {
   return (
     <>
       <Layout>
@@ -13,8 +13,9 @@ export default function Home() {
       </Layout>
     </>
   );
-}
+};
 
+export default about;
 export const query = graphql`
   query ($language: String!) {
     locales: allLocale(filter: { language: { eq: $language } }) {
