2ef0b86d460c2d3084eb557c5f4af4017148048a
arman-ag <alighanbari.1994@gmail.com>
2022-05-13 17:38:43 +0430

    feat: :art: Add footer text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <div>footer</div>
+      <div className="text-center">
+        Arman-Ag 2022
+        <span>&#169;</span>
+      </div>
     </footer>
   );
 };
