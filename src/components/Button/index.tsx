62e60bfa4d67fbf17b267ca15ad61333880f1402
arman-ag <alighanbari.1994@gmail.com>
2022-06-06 00:37:43 +0430

    fix: :bug: Fix get url  in product

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,24 @@
+import { useLocation } from '@reach/router';
 import { Link } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { buttonProps } from './types';
-const Button = ({ children, href, onClick = null, solid = false, styles }: buttonProps) => {
-  const checkParams = () => {
-    const url = location.href.split('/');
-    const param = href?.split('/');
-    return url.includes(param![2]);
-  };
 
+const Button = ({ children, href, onClick = null, solid = false, styles }: buttonProps) => {
+  const location = useLocation();
+  const checkParams = () => {
+    const url = location?.href?.split('/');
+    const param = href?.split('/');
+    return url?.includes(param![2]);
+  };
   return (
     <>
       {solid ? (
         href ? (
           <Link to={href!}>
             <button
-              className={`${styles} min-w-[18%] py-2  px-5	text-lg text-gray  border-2 rounded border-cyan-300 		 ${
-                checkParams() && `border-2 bg-cyan-300  shadow-2xl	`
-              } `}
+              className={`${styles} min-w-[18%] py-2  px-5	text-lg text-gray  border-2 rounded border-cyan-300${
+                checkParams() && `border-2 bg-cyan-300  shadow-2xl`
+              }`}
               onClick={onClick!}>
               {children}
             </button>
