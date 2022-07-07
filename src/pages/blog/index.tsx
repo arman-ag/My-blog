ad0492508a13bc6749b639809f9f7ce6556d9619
arman-ag <alighanbari.1994@gmail.com>
2022-07-07 19:46:31 +0430

    feat: :art: git add some styles in blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,9 @@
 import { graphql, Link } from 'gatsby';
 import { Helmet } from 'gatsby-plugin-react-i18next';
 import React from 'react';
+import { Trans } from 'react-i18next';
 import Layout from '../../components/layout';
+
 interface blogProps {
   [key: string]: any;
 }
@@ -13,15 +15,22 @@ const blog = ({ data }: blogProps) => {
         <title>Blog-aa-ghanbari</title>
       </Helmet>
       <Layout>
+        <div className="p-10  ">
+          <h1 className="text-3xl text-center font-bold ">
+            <Trans>blog.posts</Trans>
+          </h1>
+        </div>
         <div>
           {posts.map((post: blogProps) => (
             <article className="p-10 border-t-2 border-cyan-300" key={post.id}>
               <Link to={post.fields.slug}>
-                <h2 className="text-3xl font-bold underline">{post.frontmatter.title}</h2>
+                <h2 className="text-3xl font-bold my-3 ">{post.frontmatter.title}</h2>
               </Link>
-              <small>
-                {post.frontmatter.author}, {post.frontmatter.date}
-              </small>
+              <div className="my-2">
+                <small>
+                  {post.frontmatter.author} , {post.frontmatter.date}
+                </small>
+              </div>
               <p>{post.excerpt}</p>
             </article>
           ))}
