e1a908625c05c8d9e8668dd12434fab5bc81e64d
arman-ag <alighanbari.1994@gmail.com>
2022-07-15 18:32:04 +0430

    feat: :lipstick: style single post

--- src/components/PostSection/index.tsx
+++ src/components/PostSection/index.tsx
@@ -1,5 +1,7 @@
 import { graphql, Link, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
+
 interface blogProps {
   [key: string]: any;
 }
@@ -16,7 +18,15 @@ const Posts = () => {
           frontmatter {
             date(fromNow: true)
             title
-            author
+            excerpt
+            altFeturedImage
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
           id
@@ -27,19 +37,24 @@ const Posts = () => {
   return (
     <div>
       {posts.map((post: blogProps) => (
-        <article className="  hover:bg-hover rounded  " key={post.id}>
-          <div className="p-2 cursor-pointer">
-            <Link to={post.fields.slug}>
-              <h2 className="text-l font-bold mb-3 ">{post.frontmatter.title}</h2>
-            </Link>
-            <div className="my-2">
-              <small>
-                {post.frontmatter.author} , {post.frontmatter.date}
-              </small>
+        <div key={post.id} className="">
+          <Link to={post.fields.slug}>
+            <div className="rounded-2xl   flex items-center	flex-col m-10  hover:bg-hover  cursor-pointer 	md:flex-row">
+              <Img
+                fluid={post.frontmatter.featuredImage.childImageSharp.fluid}
+                alt={`${post.frontmatter.altFeturedImage}`}
+                className="w-40 rounded-2xl"
+              />
+
+              <div className="mx-5 flex flex-col">
+                <h2 className="text-xl font-bold my-3 ">{post.frontmatter.title}</h2>
+                <small className="text-blue">{post.frontmatter.date}</small>
+                <p className="mt-3 text-sm">{post.frontmatter.excerpt}</p>
+              </div>
             </div>
-            <p className="text-sm">{post.excerpt}</p>
-          </div>
-        </article>
+            <div className="border-t-2 border-cyan-300 w-8/12 m-auto	" />
+          </Link>
+        </div>
       ))}
     </div>
   );
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 import { Helmet } from 'gatsby-plugin-react-i18next';
 import React from 'react';
 import { Trans } from 'react-i18next';
@@ -16,23 +17,30 @@ const blog = ({ data }: blogProps) => {
       </Helmet>
       <Layout>
         <div className="p-10  ">
-          <h1 className="text-3xl text-center font-bold ">
+          <h1 className="text-3xl  font-bold ">
             <Trans>blog.posts</Trans>
           </h1>
         </div>
         <div>
           {posts.map((post: blogProps) => (
-            <article className="p-10 border-t-2 border-cyan-300" key={post.id}>
+            <div key={post.id}>
+              <div className="border-t-2 border-cyan-300 w-11/12 m-auto 	" />
               <Link to={post.fields.slug}>
-                <h2 className="text-3xl font-bold my-3 ">{post.frontmatter.title}</h2>
+                <div className=" p-10 flex items-center	flex-col 	md:flex-row">
+                  <Img
+                    fluid={post.frontmatter.featuredImage.childImageSharp.fluid}
+                    alt={`${post.frontmatter.altFeturedImage}`}
+                    className="w-56 rounded-2xl"
+                  />
+
+                  <div className="mx-5 flex flex-col">
+                    <h2 className="text-3xl font-bold my-3 ">{post.frontmatter.title}</h2>
+                    <small className="text-blue">{post.frontmatter.date}</small>
+                    <p className="mt-3">{post.frontmatter.excerpt}</p>
+                  </div>
+                </div>
               </Link>
-              <div className="my-2">
-                <small>
-                  {post.frontmatter.author} , {post.frontmatter.date}
-                </small>
-              </div>
-              <p>{post.excerpt}</p>
-            </article>
+            </div>
           ))}
         </div>
       </Layout>
@@ -51,7 +59,15 @@ export const pageQuery = graphql`
         frontmatter {
           date(fromNow: true)
           title
-          author
+          excerpt
+          altFeturedImage
+          featuredImage {
+            childImageSharp {
+              fluid(maxWidth: 400) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         excerpt
         id
