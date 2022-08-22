fecfef7aba7aa5ae3de64d8a8212f4b3e4b14ed4
arman-ag <alighanbari.1994@gmail.com>
2022-08-22 21:52:37 +0430

    feat: :sparkles: add open graph to blog page

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,10 +1,9 @@
 import { useLocation } from '@reach/router';
-import kebabCase from 'lodash/kebabCase';
-
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import { Link } from 'gatsby-plugin-react-i18next';
-import { useEffect } from 'react';
+import { Helmet, Link } from 'gatsby-plugin-react-i18next';
+import kebabCase from 'lodash/kebabCase';
+import React, { useEffect } from 'react';
 import {
   EmailIcon,
   EmailShareButton,
@@ -36,50 +35,69 @@ const blogPosts = ({ data }: any) => {
   }, []);
 
   return (
-    <Layout>
-      <div>
-        <h1 className="text-4xl font-bold">{post.frontmatter.title}</h1>
+    <>
+      <Helmet
+        title="I am a page title"
+        meta={[
+          {
+            property: `og:title`,
+            content: `${post.frontmatter.title}`
+          },
+          {
+            property: `og:type`,
+            content: `website`
+          },
+          {
+            property: `og:image`,
+            content: `${featuredImgFluid}`
+          }
+        ]}
+      />
+      <Layout>
+        <div>
+          <h1 className="text-4xl font-bold">{post.frontmatter.title}</h1>
 
-        <Img
-          alt={`${post.frontmatter.altFeturedImage}`}
-          className=" w-9/12	m-auto my-5 rounded-xl"
-          fluid={featuredImgFluid}
-        />
-        <div
-          className="prose md:prose-xl lg:prose-lg dark:prose-invert max-w-none prose-h2:font-normal	 "
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-        <div className="flex justify-around md:w-3/12 m-auto my-12">
-          <TelegramShareButton title={post.frontmatter.title} url={useLocation().href}>
-            <TelegramIcon size={32} round={true} />
-          </TelegramShareButton>
-          <WhatsappShareButton title={post.frontmatter.title} url={useLocation().href}>
-            <WhatsappIcon size={32} round={true} />
-          </WhatsappShareButton>
-          <LinkedinShareButton title={post.frontmatter.title} url={useLocation().href}>
-            <LinkedinIcon size={32} round={true} />
-          </LinkedinShareButton>
-          <PocketShareButton title={post.frontmatter.title} url={useLocation().href}>
-            <PocketIcon size={32} round={true} />
-          </PocketShareButton>
-          <EmailShareButton title={post.frontmatter.title} url={useLocation().href}>
-            <EmailIcon size={32} round={true} />
-          </EmailShareButton>
-        </div>
-        <div className="flex justify-around md:w-3/12 m-auto my-5">
-          {data.markdownRemark.frontmatter.tags.map((item: string, key: number) => (
-            <Link
-              to={`/tags/${kebabCase(item)}/`}
-              className="m-auto rounded-3xl	bg-gray-100 px-5 py-2 text-xs	font-bold	"
-              key={key}>
-              {item}
-            </Link>
-          ))}
-        </div>
+          <Img
+            alt={`${post.frontmatter.altFeturedImage}`}
+            className=" w-9/12	m-auto my-5 rounded-xl"
+            fluid={featuredImgFluid}
+          />
+          <div
+            className="prose md:prose-xl lg:prose-lg dark:prose-invert max-w-none prose-h2:font-normal	 "
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+          <div className="flex justify-around md:w-3/12 m-auto my-12">
+            <TelegramShareButton title={post.frontmatter.title} url={useLocation().href}>
+              <TelegramIcon size={32} round={true} />
+            </TelegramShareButton>
+            <WhatsappShareButton title={post.frontmatter.title} url={useLocation().href}>
+              <WhatsappIcon size={32} round={true} />
+            </WhatsappShareButton>
+            <LinkedinShareButton title={post.frontmatter.title} url={useLocation().href}>
+              <LinkedinIcon size={32} round={true} />
+            </LinkedinShareButton>
+            <PocketShareButton title={post.frontmatter.title} url={useLocation().href}>
+              <PocketIcon size={32} round={true} />
+            </PocketShareButton>
+            <EmailShareButton title={post.frontmatter.title} url={useLocation().href}>
+              <EmailIcon size={32} round={true} />
+            </EmailShareButton>
+          </div>
+          <div className="flex justify-around md:w-3/12 m-auto my-5">
+            {data.markdownRemark.frontmatter.tags.map((item: string, key: number) => (
+              <Link
+                to={`/tags/${kebabCase(item)}/`}
+                className="m-auto rounded-3xl	bg-gray-100 px-5 py-2 text-xs	font-bold	"
+                key={key}>
+                {item}
+              </Link>
+            ))}
+          </div>
 
-        <div id="inject-comments-for-uterances" />
-      </div>
-    </Layout>
+          <div id="inject-comments-for-uterances" />
+        </div>
+      </Layout>
+    </>
   );
 };
 
